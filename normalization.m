6b7caf58c9636ff0f8d346d515036c0749374f0f
DESKTOP-CFGHQQE\Nida Amalia <nidacil@gmail.com>
2016-11-29 14:53:32 +0700

    normalisasi

--- normalization.m
+++ normalization.m
@@ -1,6 +1,6 @@
 function [normalized_feature_vector,feature_vector_mean,feature_vector_std] = normalization(feature_vector)
 feature_vector_mean = mean(feature_vector);
-normalized_feature_vector = feature_vector - repmat(feature_vector_mean,[size(feature_vector,1)1]);
+normalized_feature_vector = feature_vector - repmat(feature_vector_mean,[size(feature_vector,1) 1]);
 feature_vector_std = std(feature_vector);
-normalized_feature_vector = normalized_feature_vector./repmat(feature_vector_std,[size(feature_vector,1)1]);
+normalized_feature_vector = normalized_feature_vector./repmat(feature_vector_std,[size(feature_vector,1) 1]);
 end
